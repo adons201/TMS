0f82935a826dcf4983bce12c265516f236c71b40
adons201 <adons201@yandex.ru>
2025-03-25 01:59:10 +0500

    Выправил структуру проекта, написал DOcker для создания образов. Написал compose для запуска контейнеров.

--- TMS-backend/src/main/java/ru/tms/mappers/SuiteMapper.java
+++
@@ -1,9 +0,0 @@
-package ru.tms.mappers;
-
-import org.mapstruct.Mapper;
-
-@Mapper(componentModel = "spring")
-public interface SuiteMapper {
-
-
-}
---
+++ TMS-comments/src/main/java/ru/tms/CommentsApplication.java
@@ -0,0 +1,7 @@
+package ru.tms;
+
+public class CommentsApplication {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
